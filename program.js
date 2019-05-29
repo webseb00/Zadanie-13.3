7edf96be96e1975ca251ed0cb5775a90c334478b
Sebastian Basiński <basinski.sebastian@gmail.com>
2019-05-29 12:59:05 +0200

    Create default files

---
+++ program.js
@@ -0,0 +1,36 @@
+process.stdin.setEncoding('utf-8');
+
+var userSystem = process.platform,
+    nodeVersion = process.versions.node,
+    systemLang = process.env.lang;
+
+process.stdout.write(nodeVersion);
+
+process.stdin.on('readable', function() {
+
+    var input = process.stdin.read();
+    
+    if(input !== null) {
+        var instruction = input.toString().trim();
+        if(instruction === '/exit') {
+            process.stdout.write('Quitting App!\n');
+            process.exit();
+        } else {
+            process.stderr.write('Wrong instruction!\n');
+        }
+    }
+
+    switch(userSystem) {
+        case 'linux':
+            process.stdout.write(systemLang);
+            console.log('adfd');
+            break;
+        case 'darwin':
+            process.stdout.write(systemLang);
+            console.log('adfd');
+            break;
+        default:
+            process.stdout.write(userSystem);
+    }
+
+});
